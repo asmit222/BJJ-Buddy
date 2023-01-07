e5f1f0771204e36acb1cc235f660c5f187ada9c7
Austin <asmit2222@gmail.com>
2023-01-07 13:52:00 -0500

    nm

--- src/utils/firebaseConfig/firebase.js
+++ src/utils/firebaseConfig/firebase.js
@@ -10,13 +10,13 @@ import { getFirestore } from '@firebase/firestore'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDthJ8vbHI8NHxz0Du5xNk75LsqiHYctFs',
-  authDomain: 'kindle-books-app.firebaseapp.com',
-  projectId: 'kindle-books-app',
-  storageBucket: 'kindle-books-app.appspot.com',
-  messagingSenderId: '207722033546',
-  appId: '1:207722033546:web:6b6b1c036a80cde0651873',
-  measurementId: 'G-GNN3NPPNFG'
+  apiKey: process.env.FIRESTORE_API_KEY,
+  authDomain: process.env.FIRESTORE_AUTH_DOMAIN,
+  projectId: process.env.FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIRESTORE_MESSAGING_SENDER_ID,
+  appId: process.env.FIRESTORE_APP_ID,
+  measurementId: process.env.FIRESTORE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
