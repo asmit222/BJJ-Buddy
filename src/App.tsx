b72bd171b9fa4b18bd5cc3f9b6e8baba9c823c03
Austin <asmit2222@gmail.com>
2023-01-28 17:32:47 -0500

    cache buster

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import Library from './views/Library'
 import Home from './views/Home'
 import { Route, Switch } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
-import { useScratch } from 'react-use'
-import CacheBuster from 'react-cache-buster'
-import { version } from '../package.json'
 
 const LazyCss = lazy(() => import('./components/LazyCss'))
 
@@ -16,26 +13,16 @@ const App: React.FC = () => {
 
   return (
     <React.Fragment>
-      <CacheBuster
-        currentVersion={version}
-        isEnabled={true} //If false, the library is disabled.
-        isVerboseMode={false} //If true, the library writes verbose logs to console.
-        metaFileDirectory={'.'} //If public assets are hosted somewhere other than root on your server.
-      >
-        <NavBar setClickedHomeIcon={setClickedHomeIcon} />
-        <div id='appContainer'>
-          <Switch>
-            <Route
-              path='/Library'
-              render={(props) => <Library user={user} />}
-            />
-            <Route
-              path='/'
-              render={(props) => <Home clickedHomeIcon={clickedHomeIcon} />}
-            />
-          </Switch>
-        </div>
-      </CacheBuster>
+      <NavBar setClickedHomeIcon={setClickedHomeIcon} />
+      <div id='appContainer'>
+        <Switch>
+          <Route path='/Library' render={(props) => <Library user={user} />} />
+          <Route
+            path='/'
+            render={(props) => <Home clickedHomeIcon={clickedHomeIcon} />}
+          />
+        </Switch>
+      </div>
     </React.Fragment>
   )
 }
