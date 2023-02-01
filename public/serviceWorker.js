51205485629f1a90b79412fbff16fe5481c2cb50
Austin Smith <asmit2222@gmail.com>
2023-01-31 23:10:49 -0500

    nm

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,21 +1,21 @@
 let CACHE_NAME = 'my-site-cache-v2'
-const urlsToCache = ['/', '/index.html']
+// const urlsToCache = ['/', '/index.html']
 self.addEventListener('install', function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log('Opened cache')
 
-      if (caches) {
-        // Service worker cache should be cleared with caches.delete()
-        console.log("deleting caches")
-        caches.keys().then(function(names) {
-          for (let name of names) {
-            console.log("deleting cache: " + name)
-            caches.delete(name)
-          };
-        });
-      }
+      // if (caches) {
+      //   // Service worker cache should be cleared with caches.delete()
+      //   console.log("deleting caches")
+      //   caches.keys().then(function(names) {
+      //     for (let name of names) {
+      //       console.log("deleting cache: " + name)
+      //       caches.delete(name)
+      //     };
+      //   });
+      // }
 
       return cache.addAll(urlsToCache)
     })
