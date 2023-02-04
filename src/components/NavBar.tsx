2ef72199a20e3faf20a509c8ca0dede2868f2243
Austin Smith <asmit2222@gmail.com>
2023-02-04 18:21:59 -0500

    nm

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,18 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
 
-const NavBar: React.FC = () => {
+const NavBar: React.FC = (props) => {
   const { isAuthenticated, logout, loginWithRedirect } = useAuth0()
 
   return (
     <nav className='navbar navbar-dark bg-dark'>
       <Link style={{ textDecoration: 'none' }} to='/'>
-        <div className='navBarIconAndTextDiv'>
+        <div
+          onClick={() => {
+            props.setClickedHomeIcon(true)
+          }}
+          className='navBarIconAndTextDiv'
+        >
           <span className='navbar-brand home-title-navbar'>
             <span className='navbarInnerText'>Home</span>
             <i className='fas fa-home fa-lg'></i>
