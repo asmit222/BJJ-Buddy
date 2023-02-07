1303b6986626accf6876657e55c58e3f6e4623b6
Austin <asmit2222@gmail.com>
2023-02-07 17:50:32 -0500

    nm

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,22 @@
-let CACHE_NAME = 'my-site-cache-v7'
+let CACHE_NAME = 'my-site-cache-v17'
 const urlsToCache = ['/', '/index.html']
 
 self.addEventListener('install', function (event) {
+  caches.keys().then(function (keys) {
+    keys.forEach(function (key) {
+      console.log('CacheKey: ' + key)
+      if (key !== CACHE_NAME) {
+        caches.delete(key).then(function (deleted) {
+          if (deleted) {
+            console.log(key + ' deleted successfully')
+          } else {
+            console.error(key + ' not found')
+          }
+        })
+      }
+    })
+  })
+
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
@@ -11,52 +26,53 @@ self.addEventListener('install', function (event) {
   )
 })
 
-self.addEventListener('activate', function (event) {
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (
-            CACHE_NAME !== cacheName &&
-            cacheName.startsWith('my-site-cache')
-          ) {
-            console.log('Deleting outdated cache:', cacheName)
-            return caches.delete(cacheName)
-          }
-        })
-      )
-    })
-  )
-})
+// self.addEventListener('activate', function (event) {
+//   event.waitUntil(
+//     caches.keys().then(function (cacheNames) {
+//       console.log('test')
+//       return Promise.all(
+//         cacheNames.map(function (cacheName) {
+//           if (
+//             CACHE_NAME !== cacheName &&
+//             cacheName.startsWith('my-site-cache')
+//           ) {
+//             console.log('Deleting outdated cache:', cacheName)
+//             return caches.delete(cacheName)
+//           }
+//         })
+//       )
+//     })
+//   )
+// })
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response
-      }
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       // Cache hit - return response
+//       if (response) {
+//         return response
+//       }
 
-      // Clone the request to use it multiple times
-      let fetchRequest = event.request.clone()
+//       // Clone the request to use it multiple times
+//       let fetchRequest = event.request.clone()
 
-      return fetch(fetchRequest).then(function (response) {
-        // Check if we received a valid response
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response
-        }
+//       return fetch(fetchRequest).then(function (response) {
+//         // Check if we received a valid response
+//         if (!response || response.status !== 200 || response.type !== 'basic') {
+//           return response
+//         }
 
-        // Clone the response to use it multiple times
-        let responseToCache = response.clone()
+//         // Clone the response to use it multiple times
+//         let responseToCache = response.clone()
 
-        if (event.request.url.startsWith(self.location.origin)) {
-          caches.open(CACHE_NAME).then(function (cache) {
-            cache.put(event.request, responseToCache)
-          })
-        }
+//         if (event.request.url.startsWith(self.location.origin)) {
+//           caches.open(CACHE_NAME).then(function (cache) {
+//             cache.put(event.request, responseToCache)
+//           })
+//         }
 
-        return response
-      })
-    })
-  )
-})
+//         return response
+//       })
+//     })
+//   )
+// })
