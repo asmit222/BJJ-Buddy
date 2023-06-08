9a7ea848fa2d9fc3be13591e7fd3d77fbfd39552
Austin Smith <asmit2222@gmail.com>
2023-06-08 08:59:23 -0400

    nm

--- src/utils/books.js
+++ src/utils/books.js
@@ -559,17 +559,17 @@ module.exports = {
       series: 'The Giver #1',
       genres: ['fiction'],
       filetype: 'pdf'
-    },
+    }
     // 113: {
     //   book: "Mastering Small Stakes No-Limit Hold'em: Strategies to consistently beat small stakes poker tournaments and cash games -  Jonathan Little",
     //   genres: ['non-fiction'],
     //   filetype: 'pdf'
     // }
-    114: {
-      book: 'Shatter City - Scott Westerfeld',
-      series: 'Impostors #2',
-      genres: ['fiction'],
-      filetype: 'pdf'
-    }
+    // 114: {
+    //   book: 'Shatter City - Scott Westerfeld',
+    //   series: 'Impostors #2',
+    //   genres: ['fiction'],
+    //   filetype: 'pdf'
+    // }
   }
 }
